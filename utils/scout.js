5c33d35b67a7f7ab3041f6b676f493a7450e386b
MlightShadow <rwayxu@hotmail.com>
2020-02-02 10:54:31 +0800

    format

--- utils/scout.js
+++ utils/scout.js
@@ -1,16 +1,16 @@
 module.exports = {
-  get: function (url) {
-    var request = require('request')
-    return new Promise(function (resolve, reject) {
-      request.get({
-        url: url
-      }, (error, response) => {
-        if (!error && response.statusCode === 200) {
-          resolve(response)
-        } else {
-          reject(error)
-        }
-      })
-    })
-  }
+    get: function (url) {
+        var request = require('request')
+        return new Promise(function (resolve, reject) {
+            request.get({
+                url: url
+            }, (error, response) => {
+                if (!error && response.statusCode === 200) {
+                    resolve(response)
+                } else {
+                    reject(error)
+                }
+            })
+        })
+    }
 }
