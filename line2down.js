6cb52e854a92d86534211272506bbd0500d804f9
MlightShadow <rwayxu@hotmail.com>
2020-03-13 22:44:51 +0800

    Update line2down.js

--- line2down.js
+++ line2down.js
@@ -1,5 +1,5 @@
 var fs = require('fs')
-var looter =require('./utils/looter')
+var request = require('request')
 
 const list_name = 'download.txt'
 const img_path = `./file/${new Date().getTime()}/`
@@ -12,6 +12,38 @@ if (!fs.existsSync(img_path)) {
 var data = fs.readFileSync(list_name)
 var lines = data.toString().split('\r\n')
 
+var promise_num = 0
+
+function doLoot(line) {
+    const filename = line.slice(line.lastIndexOf('/') + 1)
+    new Promise(function (resolve) {
+        promise_num++
+        console.log(`promise remain: ${promise_num} will be working`)
+        var writeStream = fs.createWriteStream(img_path + filename);
+        var readStream = request(line, {
+            timeout: 20000
+        })
+        readStream.pipe(writeStream);
+        readStream.on('error', function (err) {
+            promise_num--
+            console.log(`restart line: ${line}\nBY ERROR: ${err}\n--------`)
+            doLoot(line)
+        })
+        writeStream.on("finish", function () {
+            writeStream.end();
+            resolve(line)
+        });
+    }).then(
+        line => {
+            console.log(`COMPLETE DONE!: ${line}`)
+            promise_num--
+            if (promise_num == 0) {
+                console.log(`--------ALL DOWNLOAD COMPLETE SUCCESSFUL--------`)
+            }
+        }
+    )
+}
+
 lines.forEach(line => {
-    looter.loot(line, img_path);
+    doLoot(line);
 })
